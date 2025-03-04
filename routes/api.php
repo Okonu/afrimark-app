65404ea24a045858f9def5249c19eb136a9687cd
okonu <ianyakundi015@gmail.com>
2025-03-04 23:51:41 +0300

    update invoice routes

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,11 @@ use App\Http\Controllers\API\InvoiceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Invoice endpoints
 Route::post('invoices', [InvoiceController::class, 'store']);
 Route::get('invoices/{invoice_number}', [InvoiceController::class, 'show']);
+Route::get('invoices/by-debtor/{kra_pin}', [InvoiceController::class, 'getByDebtorKra']);
+Route::get('invoices/by-business/{kra_pin}', [InvoiceController::class, 'getByBusinessKra']);
 
 // Debtor endpoints
 Route::get('debtors', [DebtorController::class, 'index']);
